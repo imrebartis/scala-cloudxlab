15ce6c70c160ec07fbb44cccf99d649e493e429a
Imre Bartis <imre.bartis@siili.com>
2018-01-13 13:35:17 +0200

    arrays, lists & sets

--- misc.scala
+++ misc.scala
@@ -63,3 +63,47 @@ def add(x: Int, y: Int) : Int = {
 
 def add(x: Int, y: Int) = x + y // curly braces optional on single line blocks
 
+// ARRAYS
+var languages = Array("Ruby", "SQL", "Python")
+// => Array[String] = Array(Ruby, SQL, Python)
+
+languages(0)
+// => String = Ruby
+
+languages(1) = "C++"
+languages
+// Array[String] = Array(Ruby, C++, Python)
+
+for (x <- languages) {
+      println(x)
+      } 
+// Ruby
+// C++
+// Python
+
+//LISTS
+//List in Scala represents a linked list having 
+//elements such that each element has a value and pointer to the next element.
+
+var number_list = List(1,2,3)
+// => List[Int] = List(1, 2, 3)
+
+number_list :+ 4
+/ => List[Int] = List(1, 2, 3, 4)
+
+number_list(1) = 7 // lists are immutable
+// => <console>:13: error: value update is not a member of List[Int]
+//      number_list(1) = 7
+
+//SETS
+// A set in Scala is a bag of data with no duplicates.
+// Also, the ordering is not guaranteed in sets.
+
+var set = Set(76, 5, 9, 1, 2)
+// => scala.collection.immutable.Set[Int] = Set(5, 1, 9, 2, 76)
+
+set = set + 20
+// => scala.collection.immutable.Set[Int] = Set(5, 20, 1, 9, 2, 76)
+
+set(5)
+// => Boolean = true (since 5 is in the set)
